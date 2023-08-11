97888dbedf12fb8ff8af197400293ace9c69f9f1
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-10 19:48:34 -0700

    com101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include<unistd.h>
 
 /**
  * main - Entry point
@@ -9,7 +8,8 @@
 
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n");
+	char str []="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,str,59);
 	return (0);
 
 }
