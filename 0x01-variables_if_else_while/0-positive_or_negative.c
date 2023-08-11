e0045e15deb54c93d7bd0d3457c993c084426c1e
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-11 13:25:34 -0700

    com0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,19 +11,10 @@ int main(void)
 int n;
 scanf("%d", &n);
 if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else
-{
-	if (n < 0)
-	{
+	printf("%d is positive\n", n);
+else if (n < 0)
 	printf("%d is negative\n", n);
-	}
-	else
-	{
+else
 	printf("%d is zero\n", n);
-	}
-}
 return (0);
 }
