d363126e15657bfefc71fd373340eac5ee50da85
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-11 18:08:16 -0700

    com7

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,11 @@
 
 int main(void)
 {
-for (int i = 0; i <= 9; i++)
+	int i;
+	char x;
+for (i = 0; i <= 9; i++)
 	putchar((i % 10) + '0');
-for (char x = 'a'; x <= 'f'; i++)
+for (x = 'a'; x <= 'f'; i++)
 	putchar(x);
 putchar('\n');
 	return (0);
