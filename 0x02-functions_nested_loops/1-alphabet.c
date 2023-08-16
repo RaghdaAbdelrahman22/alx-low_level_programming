6a15f7e9134e092ae183227840fda6ccc1029f73
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-15 17:18:49 -0700

    com1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
  *Describtion: print_alphabet - prints lowercase alphabet ended by new line.
  *
- * Return: Always 0.
  */
 
 	void print_alphabet(void)
