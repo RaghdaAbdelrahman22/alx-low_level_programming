5fd8caf38592839f5fc073cd64845d0c4561c850
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-15 19:58:01 -0700

    com11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,23 +8,16 @@
 void print_to_98(int n)
 {
 if (n >= 98)
-{
-	do
-		n--;
-	while (n > 98)
-		_putchar("%d");
-		_putchar(' ');
-_putchar("%d\n");
-_putchar(n);
-}
-else
-{
-	do
-		n++;
-	while (n < 98)
-		_putchar("%d");
-		_putchar(' ');
-_putchar("%d\n");
-_putchar(n);
-}
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
