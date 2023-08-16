51d5ae60352212afe2ac1cd74343d2ade702ae65
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-15 17:41:54 -0700

    com2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10- prints 10 times lowercase alphabet-new line.
+*
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	while (i++  < 10)
+	{
+		for (c = 'a'; c = 'z'; c++)
+			putchar(c);
+		putchar('\n');
+	}
+}
