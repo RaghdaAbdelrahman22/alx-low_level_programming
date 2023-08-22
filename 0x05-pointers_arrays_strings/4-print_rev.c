8807db605358a077e9cae1b9c65afb7cd51b5664
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-22 04:39:12 +0200

    c4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev-print sting in reverse and newline.
+ *@s: string printed in reverse.
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+	while (s[l])
+		i++;
+	while(l--)
+		putchar(s[l]);
+
+	putchar('\n');
+}
