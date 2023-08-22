7233983f1b5ffe989a616779e38621c15bbba2b7
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-22 05:10:13 +0200

    c4

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *puts2- prints every character of a string followed by newline.
+ *@str: string printed.
+ */
+void _puts(char *str)
+{
+	int i = 0, l = 0;
+	while (str[i])
+		l++;
+	for (i = 0; i < l; i++) 
+		putchar(str[i]);
+	
+	putchar('\n');
+}
+
