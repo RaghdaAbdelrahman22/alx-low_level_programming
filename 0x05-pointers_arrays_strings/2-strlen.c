f0d9ab97cf79602c6e118756ebad574db47d3cdc
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-08-22 04:22:25 +0200

    c2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen-return length of string.
+ *@s: string counted.
+ * 
+ *Return: length of string @s.
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	for (length  = 0; *s != '\0'; s++)
+		++length;
+	return (length);
+}
