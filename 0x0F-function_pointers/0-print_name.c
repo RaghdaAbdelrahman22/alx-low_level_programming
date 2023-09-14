559cc2ef185afdb1106f39464de2032950ce9822
RaghdaAbdelrahman22 <raghdaelsayedabdelrahman@example.com>
2023-09-14 02:54:08 +0200

    com0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 #include "function_pointers.h"
 
 /**
@@ -7,7 +7,6 @@
  *
  * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
